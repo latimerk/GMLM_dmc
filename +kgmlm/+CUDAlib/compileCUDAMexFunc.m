23aced63255fbe5b70264f1caca75a5dcdb3b37a
latimerk <latimerkw@gmail.com>
2021-12-14 16:43:42 -0600

    cleaning up compiler code a little

--- +kgmlm/+CUDAlib/compileCUDAMexFunc.m
+++ +kgmlm/+CUDAlib/compileCUDAMexFunc.m
@@ -1,19 +1,23 @@
-function [fNames, fNames_grp] = compileCUDAMexFunc(fName)
+function [fNames, fNames_grp] = compileCUDAMexFunc(lib_num)
 
 fNames_grp = {["kcGMLM_mex_create.cpp", "kcGMLM_mex_clear.cpp",  "kcGMLM_mex_computeLL.cpp", "kcGMLM_mex_computeLL_async.cpp", "kcGMLM_mex_computeLL_gather.cpp"], ...
               ["kcGLM_mex_create.cpp", "kcGLM_mex_clear.cpp",  "kcGLM_mex_computeLL.cpp"], ...
               ["kcGMLMPop_mex_create.cpp", "kcGMLMPop_mex_clear.cpp",  "kcGMLMPop_mex_computeLL.cpp", "kcGMLMPop_mex_computeLL_async.cpp", "kcGMLMPop_mex_computeLL_gather.cpp"]};
 
 fNames = [fNames_grp{:}];
+
+if(nargout > 0)
+    return;
+end
+
+if(nargin > 0 && (~isempty(lib_num) && isnumeric(lib_num)))
+    fNames = fNames_grp{lib_num};
+elseif(nargin > 0)
+    fNames = lib_num;
+end
       
-if(nargin > 0 && numel(fName) > 1)
-    for ii = 1:numel(fName)
-        kgmlm.CUDAlib.compileCUDAMexFunc(fName(ii));
-    end
-elseif(nargin > 0 && ~isempty(fName))
-    if(isnumeric(fName))
-        fName = fNames(fName);
-    end
+
+if(nargin > 0 && numel(fNames) == 1)
     [~, ~, ~, ~, sourceDir, objDir] = kgmlm.CUDAlib.myCUDAPaths();
         
     if(~isfolder(sourceDir))
@@ -25,8 +29,8 @@ elseif(nargin > 0 && ~isempty(fName))
     
     compileCUDAlibMex = @(fName) mex('-c', sprintf('%s/%s',sourceDir,fName), '-outdir', objDir, sprintf('-I%s', sourceDir));%,'COMPFLAGS=$COMPFLAGS /std:c++14'
     
-    compileCUDAlibMex(fName );
-elseif((nargin == 0 || isempty(fName)) && nargout == 0)
+    compileCUDAlibMex(fNames);
+else
     for ii = 1:numel(fNames)
         kgmlm.CUDAlib.compileCUDAMexFunc(fNames(ii));
     end
