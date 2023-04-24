dda629afd4a23c894ea4e8d66ce4d54354fa30b4
Kenneth Latimer <latimerkw@gmail.com>
2023-04-24 13:50:26 -0500

    fixing install instructions

--- CUDA_mex_instructions.m
+++ CUDA_mex_instructions.m
@@ -2,9 +2,8 @@
 % requires CUDA version 11.0
 
 % on Ubuntu or Windows (Linux commands may work similarly on MAC, but never attempted)
-% first, check paths in code/CUDAlib/myCUDAPaths.m
+% first, check paths in +kgmlm/+CUDAlib/myCUDAPaths.m
 % then run
-%     addGMLMpaths;
-%     buildGMLMCUDALibrary;
+%     kgmlm.CUDAlib.buildGMLMCUDALibrary;
 %
 %       If a link error occurs, try 'clear mex' and then link again.
